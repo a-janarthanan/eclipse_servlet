a6950d3e2ed73fd4c60f5746aefc909eeed18bb6
Janarthanan <jana12raz@gmail.com>
2019-07-15 16:06:30 +0530

    changed for working

--- src/defaultpackage/ExampleGeneric.java
+++ src/defaultpackage/ExampleGeneric.java
@@ -5,12 +5,15 @@ import javax.servlet.*;
 import java.io.*; 
 
 public class ExampleGeneric extends HttpServlet{ 
+
+	private static final long serialVersionUID = -8852609235235544061L;
+
+	@Override
 	   public void doGet(HttpServletRequest req,HttpServletResponse res) 
 	   throws ServletException,IOException 
 	   {  
 	     res.setContentType("text/html"); 
 	     PrintWriter pwriter=res.getWriter(); 
-
 	     String name = req.getParameter("uname");
 	     String age = req.getParameter("uage");
 	     pwriter.println("Name: "+name); 
